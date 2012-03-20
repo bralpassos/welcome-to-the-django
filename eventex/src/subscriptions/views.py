bfdb1885e3ccd760c1d5c8e93e6aac6710f29c83
Bruno Almeida <brunoalmeidapassos@gmail.com>
2012-03-20 20:04:05 -0300

    Criado template base e o formulário ainda não está funcionando

--- eventex/src/subscriptions/views.py
+++ eventex/src/subscriptions/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+from django.shortcuts   import render_to_response
+from django.template    import RequestContext
+from forms              import SubscriptionForm
+
+def subscribe(request):
+    context = RequestContext(request, {'form' : SubscriptionForm()})
+
+    return render_to_response('subscriptions/new.html', context)
