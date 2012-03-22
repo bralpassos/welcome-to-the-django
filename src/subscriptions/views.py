8401353bb9bd82c0c588913a62ea875643c7fc21
Bruno Almeida <brunoalmeidapassos@gmail.com>
2012-03-22 19:25:26 -0300

    Envio de e-mail após o cadastro na página já está funcional!

--- src/subscriptions/views.py
+++ src/subscriptions/views.py
@@ -1,6 +1,12 @@
+# coding: utf-8
+
+from django.core.mail   import send_mail
 from django.http        import HttpResponseRedirect, HttpResponse
-from django.shortcuts   import render_to_response
+from django.shortcuts   import render_to_response, get_object_or_404
 from django.template    import RequestContext
+from django.conf        import settings
+
+from models             import Subscriptions
 from forms              import SubscriptionForm
 
 def subscribe(request):
@@ -13,6 +19,13 @@ def subscribe(request):
         if form.is_valid():
             subscriptions = form.save()
 
+            # Envia o e-mail de notificação
+            send_mail(subject=u'Eventex - Cadastro realizado com sucesso!',
+                message=u'Obrigado pela sua inscrição!',
+                from_email=settings.DEFAULT_FROM_EMAIL,
+                recipient_list=[subscriptions.email]
+            )
+
             return HttpResponseRedirect('%i/sucesso/' % subscriptions.pk)
 
 
@@ -21,5 +34,8 @@ def subscribe(request):
     return render_to_response('subscriptions/new.html', context)
 
 
-def success(request):
-    return HttpResponse('Sucesso!')
+def success(request, pk):
+    subscription = get_object_or_404(Subscriptions, pk=pk)
+    context = RequestContext(request, {'subscription': subscription})
+
+    return render_to_response('subscriptions/success.html', context)
