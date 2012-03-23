34e17ba54a03ee8811147fab7fb9f519b15caca6
Bruno Almeida <brunoalmeidapassos@gmail.com>
2012-03-23 00:50:23 -0300

    Habilitado a app admin.

--- src/urls.py
+++ src/urls.py
@@ -1,14 +1,16 @@
 from django.conf.urls.defaults  import patterns, include, url
+from django.contrib             import admin
+
 from core.views                 import homepage
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+admin.autodiscover()
 
 urlpatterns = patterns('',
     (r'^$', homepage),
     (r'^inscricao/', include('subscriptions.urls', namespace='subscriptions')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
+
+
