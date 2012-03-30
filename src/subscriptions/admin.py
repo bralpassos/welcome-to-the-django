847241c55569a2c6d202f6db362b6eb367130687
Bruno Almeida <brunoalmeidapassos@gmail.com>
2012-03-30 02:20:05 -0300

    Opção para baixar toda a lista de inscritos no formato csv
    Falta apenas colocar o link na página do admin, estou perdito com a
    localzaçao do template e a referência à página via namespace no href.

--- src/subscriptions/admin.py
+++ src/subscriptions/admin.py
@@ -2,6 +2,8 @@
 
 import datetime
 
+from django.conf.urls.defaults  import patterns, url
+from django.http                import HttpResponse
 from django.contrib             import admin
 from django.utils.translation   import ugettext as _
 from django.utils.translation   import ungettext
@@ -32,12 +34,36 @@ class SubscriptionAdmin(admin.ModelAdmin):
 
     mark_as_paid.short_description = _(u'Marcar como paga')
 
-
     # Nova coluna que informa se o usúario foi criado hoje ou não
     def subscribed_today(self, obj):
         return obj.created_at.date() == datetime.date.today()
 
     subscribed_today.short_description = u'Inscrito hoje?'
     subscribed_today.boolean = True     # Exibe um ícone ao invés de 'True' ou 'False'
+
+
+    # Opção para exportar todas as inscrções no formato csv
+    def export_subscriptions(self, request):
+        subscriptions = self.model.objects.all()
+        rows = [','.join([s.name, s.email]) for s in subscriptions]
+
+        response = HttpResponse('\r\n'.join(rows))
+        response.mimetype = 'text/csv'
+        response['Content-Disposition'] = 'attachment; filename=inscricoes.csv'
+
+        return response
+
+
+    def get_urls(self):
+        original_urls = super(SubscriptionAdmin, self).get_urls()
+        extra_url = patterns('',
+            url(r'exportar-inscricoes/$',
+                self.admin_site.admin_view(self.export_subscriptions),
+                name='export_subscriptions'
+            )
+        )
+
+        return extra_url + original_urls
+
 
 admin.site.register(Subscriptions, SubscriptionAdmin)
