af5dc51e0b2e90f2074bafdff8fce10336b572d5
Bruno Almeida <brunoalmeidapassos@gmail.com>
2012-03-30 01:14:29 -0300

    Opção para marcar uma inscrição como paga via admin.

--- src/subscriptions/admin.py
+++ src/subscriptions/admin.py
@@ -1,15 +1,37 @@
-# coding: utf-8
+# -*- coding: utf-8 -*-
 
 import datetime
 
-from django.contrib         import admin
-from subscriptions.models   import Subscriptions
+from django.contrib             import admin
+from django.utils.translation   import ugettext as _
+from django.utils.translation   import ungettext
+
+from subscriptions.models       import Subscriptions
 
 class SubscriptionAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'phone', 'created_at', 'subscribed_today')
+    list_display = ('name', 'email', 'phone', 'created_at', 'subscribed_today', 'paid')
+    list_filter = ['created_at', 'paid']
+
     date_hierarchy = 'created_at'
-    list_filter = ['created_at']
     search_fields = ('name', 'cpf', 'phone', 'email', 'created_at')
+
+    actions = ['mark_as_paid']
+
+
+    # Opção para marcar a inscrição como paga via admin
+    def mark_as_paid(self, request, queryset):
+        count = queryset.update(paid=True)
+
+        msg = ungettext(
+            u'%(count)d inscrição foi marcada com paga.',
+            u'%(count)d inscrições foram marcadas como pagas.',
+            count
+        ) % {'count': count}
+
+        self.message_user(request, msg)
+
+    mark_as_paid.short_description = _(u'Marcar como paga')
+
 
     # Nova coluna que informa se o usúario foi criado hoje ou não
     def subscribed_today(self, obj):
